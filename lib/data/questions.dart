ea32e8364d67800c989d2c86f80abbd37dfdea0a
Hajrah Hameed <hajrahhameed56@gmail.com>
2026-02-12 11:34:23 -0500

    Created Question data model and list of question data

---
+++ lib/data/questions.dart
@@ -0,0 +1,24 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'How did Edward Elric lose his arm in FMA?'
+    ,
+    [
+      'Putting his brothers sould in a suit of armor',
+      'In a Duel',
+      'In a freak Dabbing Accident',
+      'He never had any arms', 
+    ]
+  ),
+  QuizQuestion(
+    'Which of these are a horror movie?'
+    ,
+    [
+      'Conjuring',
+      'Insidious',
+      'Mean Girls',
+      'Annabelle', 
+    ]
+  ),
+];
